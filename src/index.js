b82d826795701db0ea63aa5fedd5d504c4b4f88f
MickaelPuygcerver <mickael.andrade@peakinvest.com.br>
2020-05-30 21:30:03 -0300

    usando state no componente pai

--- src/index.js
+++ src/index.js
@@ -1,17 +1,80 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class Casa extends React.Component {
+    render(){
+        return (
+            <button className="casa" onClick={() => this.props.onClick()} >
+                {this.props.model}
+            </button>
+        );
+    }
+}
+
+
+class Tabuleiro extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = { 
+            casas: new Array(9).fill(null)
+        };
+    }
+
+    clickCasa(numeroDaCasa){
+        let casas = this.state.casas.slice();
+        casas[numeroDaCasa] = 'X';
+
+        this.setState({casas: casas});
+    }
+
+    renderizarCasa(numeroDaCasa){
+        return <Casa onClick={() => this.clickCasa(numeroDaCasa)} 
+                    model={this.state.casas[numeroDaCasa]}/>;
+    }
+
+    render() {
+        const status = 'Next player: X';
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderizarCasa(0)}
+                    {this.renderizarCasa(1)}
+                    {this.renderizarCasa(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderizarCasa(3)}
+                    {this.renderizarCasa(4)}
+                    {this.renderizarCasa(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderizarCasa(6)}
+                    {this.renderizarCasa(7)}
+                    {this.renderizarCasa(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Jogo extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Tabuleiro />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Jogo></Jogo>,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
